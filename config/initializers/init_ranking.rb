1362e3a060da85962ab4fd43f334a0652b970161
Steve Cho <imstevecho@yahoo.com>
2017-04-16 22:59:03 -0400

    switched to postgres

--- config/initializers/init_ranking.rb
+++ config/initializers/init_ranking.rb
@@ -1,5 +1,7 @@
 # Initialize Ranking for the first time
-(User.all.pluck(:id) - Ranking.all.pluck(:user_id)).each do |user_id|
-  puts "creating ranking for #{user_id} ........"
-  Ranking.create(user_id: user_id, score: 0, games_played: 0)
+if (User.table_exists? && Ranking.table_exists?)
+  (User.all.pluck(:id) - Ranking.all.pluck(:user_id)).each do |user_id|
+    puts "creating ranking for #{user_id} ........"
+    Ranking.create(user_id: user_id, score: 0, games_played: 0)
+  end
 end
